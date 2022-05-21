3754ce8f852e2f6b47f7c5c5596e88e137401674
Leonid <x.worklene@gmail.com>
2022-05-21 22:54:03 +0300

    Добавил рисование

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,38 +15,75 @@ using System.Windows.Shapes;
 
 namespace graphical_editor
 {
-    
+    enum StrokeColor
+    {
+        Black, Red, Blue, Yellow, Green
+    }
+
+    enum DrawMode
+    {
+        Ellipse, Pen, Pencil
+    }
+
+    enum Thickness
+    {
+        SMALL, MEDIUM, BIG
+    }
+
     public partial class MainWindow : Window
     {
         DrawMode mode;
         Thickness thickness;
-        StrokeColor color; 
-
-        enum StrokeColor
-        {
-            Black, Red, Blue, Yellow, Green
-        }
-
-        enum  DrawMode
-        {
-             Ellipse, Pen, Pencil
-        }
-
-        enum Thickness
-        {
-            SMALL, MEDIUM , BIG 
-        }
-
+        StrokeColor color;
+        Point currentPosition; 
+        Line drawLine; 
 
         public MainWindow()
         {
-            mode = DrawMode.Pen; 
+            color = StrokeColor.Red;
+            mode = DrawMode.Pen;
+            thickness = Thickness.MEDIUM;
+
             InitializeComponent();
         }
 
-        
-        
+       
 
+        private void penMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mode = DrawMode.Pen; 
+        }
 
+        private void pencilMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mode = DrawMode.Pencil;
+        }
+
+        private void drawCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                drawLine = new Line();
+                Setup.setupLineColor(drawLine, color);
+                Setup.setupLineThickness(drawLine, thickness);
+
+                drawLine.X1 = currentPosition.X;
+                drawLine.Y1 = currentPosition.Y;
+                drawLine.X2 = e.GetPosition(drawCanvas).X;
+                drawLine.Y2 = e.GetPosition(drawCanvas).Y;
+
+                currentPosition = e.GetPosition(drawCanvas);
+
+                drawCanvas.Children.Add(drawLine);
+            }
+        }
+
+        private void drawCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState == MouseButtonState.Pressed)
+            {
+                currentPosition = e.GetPosition(drawCanvas);
+            }
+        }
     }
 }
---
+++ Setup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace graphical_editor
+{
+    internal class Setup
+    {
+        static public void setupLineColor(Line line, StrokeColor color)
+        {
+            switch (color)
+            {
+                case StrokeColor.Black:
+                    line.Stroke = new SolidColorBrush(Colors.Black);
+                    return;
+                case StrokeColor.Red:
+                    line.Stroke = new SolidColorBrush(Colors.Red);
+                    return;
+                case StrokeColor.Blue:
+                    line.Stroke = new SolidColorBrush(Colors.Blue);
+                    return;
+                case StrokeColor.Yellow:
+                    line.Stroke = new SolidColorBrush(Colors.Yellow);
+                    return;
+                case StrokeColor.Green:
+                    line.Stroke = new SolidColorBrush(Colors.Green);
+                    return;
+            }
+
+
+        }
+
+        static public void setupLineThickness(Line line , Thickness thickness)
+        {
+            switch (thickness)
+            {
+                case Thickness.SMALL:
+                    line.StrokeThickness = 2;
+                    return;
+                case Thickness.MEDIUM:
+                    line.StrokeThickness = 4;
+                    return;
+                case Thickness.BIG:
+                    line.StrokeThickness = 6;
+                    return; 
+            }
+        }
+    }
+}
