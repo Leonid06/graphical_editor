27dd71ceedd8c626d4c7e9804dc48adee973a661
IvanVybornov <vybornov.ivan@gmail.com>
2022-05-27 19:18:30 +0400

    Ломанное рисование эллипса

--- builders/EllipseBuilder.cs
+++ builders/EllipseBuilder.cs
@@ -14,7 +14,8 @@ namespace graphical_editor
 {
      internal class EllipseBuilder
     {
-   
+
+        static Point currentPosition;
 
         static public void createPenEllipse(
             double thickness,
@@ -49,24 +50,41 @@ namespace graphical_editor
 
         static public void createDemoEllipse(
             double thickness,
+            Color color,
             Canvas canvas,
-            Color color)
+            MouseEventArgs e
+            )
         {
             Ellipse el = new Ellipse();
 
-            el.StrokeThickness = thickness; 
-         
-            el.Stroke = new SolidColorBrush(color);
-            el.Fill = new SolidColorBrush(color);
+            el.StrokeThickness = thickness;
 
-            Canvas.SetTop(el, canvas.Height / thickness / 2);
-            Canvas.SetLeft(el, canvas.Width / thickness / 2);
-            
+            el.Stroke = new SolidColorBrush(color);
+            el.Fill = new SolidColorBrush(Colors.White);
+
+            Canvas.SetTop(el, currentPosition.Y);
+            Canvas.SetLeft(el, currentPosition.X);
+
+            el.Height = Abs(currentPosition.Y - e.GetPosition(canvas).Y);
+            el.Width = Abs(currentPosition.X - e.GetPosition(canvas).X);
+
+            //Canvas.SetTop(el, canvas.Height / thickness / 2);
+            //Canvas.SetLeft(el, canvas.Width / thickness / 2);
 
             canvas.Children.Add(el);
         }
 
-       
+        private static double Abs(double v)
+        {
+            throw new NotImplementedException();
+        }
+
+        static public void SetCurrentPosition(
+            Canvas canvas,
+            MouseEventArgs e)
+        {
+            currentPosition = e.GetPosition(canvas);
+        }
     }
 
     
